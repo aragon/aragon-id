9e2240706e745cd6c7b80efcc9f4ecc6dd20889c
Jorge Izquierdo <jorge@izqui.me>
2018-03-05 13:41:00 +0100

    Deploy to devnet

--- truffle.js
+++ truffle.js
@@ -19,6 +19,11 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     },
+    devnet: {
+      host: "devnet",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
     rinkeby: {
       network_id: 4,
       provider: rinkebyProvider,
