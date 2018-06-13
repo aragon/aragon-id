30fd8475f9e26e8ff5f6d2ee601e8a212f31668c
Brett Sun <qisheng.brett.sun@gmail.com>
2018-06-13 17:14:18 +0200

    Fix tests from missing imports and babel-register (#15)

--- test/ens/ENS.sol
+++ test/ens/ENS.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.0;
 
-import "../../contracts/ens/AbstractENS.sol";
+import "@aragon/os/contracts/lib/ens/AbstractENS.sol";
 
 
 /**
--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+require('babel-core/register')
+require('babel-polyfill')
+
 const HDWalletProvider = require('truffle-hdwallet-provider')
 
 let rinkebyProvider = {}
