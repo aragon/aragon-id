b26bc79effe755f2bac690ab0533c1700d8183ce
Brett Sun <qisheng.brett.sun@gmail.com>
2018-07-26 23:31:01 +0200

    chore: fix deployment addresses (#19)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   rinkeby: {
-    ens: "0xaa0ccb537289d226941745c4dd7a819a750897d0",
-    aragonid: "0x9f4984865797213dd4431551114c47bd98be8aac"
+    ens: "0xfbae32d1cde62858bc45f51efc8cc4fa1415447e",
+    aragonid: "0x3a06a6544e48708142508d9042f94ddda769d04f"
   }
 }
