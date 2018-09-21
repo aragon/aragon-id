fbf367a637c7c2094b1d335516422e5c57a04ae0
ßingen <bingentxu@gmail.com>
2018-09-21 20:33:26 +0200

    Update deploy script

--- scripts/deploy-beta-aragonid.js
+++ scripts/deploy-beta-aragonid.js
@@ -1,29 +1,43 @@
 const namehash = require('eth-ens-namehash').hash
 const keccak256 = require('js-sha3').keccak_256
 
-const FIFSResolvingRegistrar = artifacts.require('FIFSResolvingRegistrar')
-const ENS = artifacts.require('AbstractENS')
-
-const owner = process.env.OWNER || '0x4cb3fd420555a09ba98845f0b816e45cfb230983'
-const ens = process.env.ENS || '0xfbae32d1cde62858bc45f51efc8cc4fa1415447e'
+const globalArtifacts = this.artifacts // Not injected unless called directly via truffle
+const defaultOwner = process.env.OWNER || '0x4cb3fd420555a09ba98845f0b816e45cfb230983'
+const defaultENSAddress = process.env.ENS || '0xfbae32d1cde62858bc45f51efc8cc4fa1415447e'
 
 const tld = namehash('eth')
 const label = '0x'+keccak256('aragonid')
 const node = namehash('aragonid.eth')
 
-module.exports = async callback => {
-  const publicResolver = await ENS.at(ens).resolver(namehash('resolver.eth'))
-  console.log('deploying AragonID')
-  const aragonID = await FIFSResolvingRegistrar.new(ens, publicResolver, node)
+module.exports = async (
+  truffleExecCallback,
+  {
+    artifacts = globalArtifacts,
+    ensAddress = defaultENSAddress,
+    owner = defaultOwner,
+    verbose = true
+  } = {}
+) => {
+  const log = (...args) => {
+    if (verbose) { console.log(...args) }
+  }
 
-  console.log('assigning ENS name to AragonID')
-  await ENS.at(ens).setSubnodeOwner(tld, label, aragonID.address)
+  log(ensAddress, owner)
+  const FIFSResolvingRegistrar = artifacts.require('FIFSResolvingRegistrar')
+  const ENS = artifacts.require('AbstractENS')
 
-  console.log('assigning owner name')
+  const publicResolver = await ENS.at(ensAddress).resolver(namehash('resolver.eth'))
+  log('deploying AragonID')
+  const aragonID = await FIFSResolvingRegistrar.new(ensAddress, publicResolver, node)
+
+  log('assigning ENS name to AragonID')
+  await ENS.at(ensAddress).setSubnodeOwner(tld, label, aragonID.address)
+
+  log('assigning owner name')
   await aragonID.register('0x'+keccak256('owner'), owner)
 
-  console.log('===========')
-  console.log('Deployed AragonID:', aragonID.address)
+  log('===========')
+  log('Deployed AragonID:', aragonID.address)
 }
 
 // Deployed AragonID: 0x3a06a6544e48708142508d9042f94ddda769d04f
