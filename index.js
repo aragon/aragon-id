f77806d7be2a4e547e01047c5389a67b337c9a88
Brett Sun <qisheng.brett.sun@gmail.com>
2018-10-29 10:59:01 +0100

    Add deployment details

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 module.exports = {
+  mainnet: {
+    ens: "0x314159265dd8dbb310642f98f50c066173c1259b",
+    aragonid: "0x546aa2eae2514494eeadb7bbb35243348983c59d"
+  },
   rinkeby: {
+    ens: "0x98df287b6c145399aaa709692c8d308357bc085d",
+    aragonid: "0x3665e7bfd4d3254ae7796779800f5b603c43c60d"
+  },
+  "rinkeby-old": {
     ens: "0xfbae32d1cde62858bc45f51efc8cc4fa1415447e",
     aragonid: "0x3a06a6544e48708142508d9042f94ddda769d04f"
+  },
+  staging: {
+    ens: "0xfe03625ea880a8cba336f9b5ad6e15b0a3b5a939",
+    aragonid: "0xa2e76aa249e3f52ea94c175662adadf27185484a"
   }
 }
